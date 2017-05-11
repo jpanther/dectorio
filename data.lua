87608ee7771b3d21f38dca2542269eb44b94fbad
jpanther <jpanther@users.noreply.github.com>
2017-05-11 14:52:57 +1000

    Add license bits

--- data.lua
+++ data.lua
@@ -1,3 +1,25 @@
+--------------------------
+-- COPYRIGHT & LICENSE  --
+--------------------------
+
+-- Dectorio, a Factorio mod
+-- Copyright (c) 2017 James "PantherX" Panther
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- A copy of the GNU General Public License is packaged along with
+-- this program.  You can also see <http://www.gnu.org/licenses/>.
+
+--------------------------
+
 --data.lua
 
 require("prototypes.entity.entities")
