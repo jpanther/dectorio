577aeba36193bcccfed4dd1ab4ff7f070c88f9d7
jpanther <jpanther@users.noreply.github.com>
2018-01-15 12:36:39 +1100

    Add support for Alien Biomes v3 - Fixes #59

--- control.lua
+++ control.lua
@@ -160,15 +160,11 @@ local function on_configuration_changed(data)
 			for _,force in pairs(game.forces) do
 				local tech = force.technologies
 				local rec = force.recipes
-				if tech["dect-landscaping"].researched and rec["dect-alien-biomes-grass-red"] then
-					local alien_biomes = {
-						"grass-red","grass-orange","grass-yellow","grass-yellow-fade","grass-dry","grass-medium","grass","grass-blue-fade","grass-blue","grass-purple-fade","grass-purple",
-						"dirt-red","dirt-brown","dirt-tan","dirt","dirt-dull","dirt-grey","dirt-red-dark","dirt-brown-dark","dirt-tan-dark","dirt-dark","dirt-dull-dark","dirt-grey-dark",
-						"sand-red","sand-orange","sand-gold","sand","sand-dull","sand-grey","sand-red-dark","sand-orange-dark","sand-gold-dark","sand-dark","sand-dull-dark","sand-grey-dark",
-						"snow","volcanic-cool","volcanic-medium","volcanic-hot"
-					}
-					for _, tile in pairs(alien_biomes) do
-						rec["dect-alien-biomes-"..tile].enabled = true
+				if tech["dect-landscaping"].researched and settings.startup["alien-biomes-terrain-scale"] then
+					for _, tile in pairs(DECT.CONFIG.ALIEN_BIOMES) do
+						if rec["dect-alien-biomes-"..tile] then
+							rec["dect-alien-biomes-"..tile].enabled = true
+						end
 					end
 					notification({"dect-notify.supported-mod-added", {"dect-notify.dectorio"}, "Alien Biomes"})
 				end
