384db7b4e8df7fc79e1fa0bdf2a0e1f0180806b0
Aidiakapi <aidiakapi@gmail.com>
2018-04-21 14:45:45 +0200

    Fix 0.8.6's migration when using Alien Biomes.

--- migrations/0.8.6.lua
+++ migrations/0.8.6.lua
@@ -9,9 +9,14 @@ for index, force in pairs(game.forces) do
 	if settings.startup["dectorio-waterfill"].value then
 		if tech["landfill"].researched then
 			rec["dect-base-water"].enabled = true
-			rec["dect-base-water-green"].enabled = true
 			rec["dect-base-deepwater"].enabled = true
-			rec["dect-base-deepwater-green"].enabled = true
+			
+			-- When Alien Biomes is activated, the green variations of
+			-- the water aren't created by Dectorio.
+			if not settings.startup["alien-biomes-terrain-scale"] then
+				rec["dect-base-water-green"].enabled = true
+				rec["dect-base-deepwater-green"].enabled = true
+			end
 		end
 	end
 end
