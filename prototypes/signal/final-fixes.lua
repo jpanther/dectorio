2202201e072bacdcc9adcd4f37d99518ae846c14
James Panther <4462786+jpanther@users.noreply.github.com>
2019-03-01 18:52:05 +1100

    Added new signal colours

--- prototypes/signal/final-fixes.lua
+++ prototypes/signal/final-fixes.lua
@@ -16,4 +16,26 @@ if DECT.ENABLED["signals"] then
 	local lamp_glow = data.raw.lamp["dect-small-lamp-glow"]
 	lamp_glow.signal_to_color_mapping = colors
 
+	-- Reorder some of the base game signals to improve the signal layout
+	data:extend({
+		{
+			type = "item-subgroup",
+			name = "last",
+			group = "signals",
+			order = "zz"
+		}
+	})
+	if data.raw["virtual-signal"]["signal-check"] then
+		data.raw["virtual-signal"]["signal-check"].subgroup = "last"
+		data.raw["virtual-signal"]["signal-check"].order = "z[misc]-[9checked]"
+	end
+	if data.raw["virtual-signal"]["signal-info"] then
+		data.raw["virtual-signal"]["signal-info"].subgroup = "last"
+		data.raw["virtual-signal"]["signal-info"].order = "z[misc]-[9info]"
+	end
+	if data.raw["virtual-signal"]["signal-dot"] then
+		data.raw["virtual-signal"]["signal-dot"].subgroup = "last"
+		data.raw["virtual-signal"]["signal-dot"].order = "z[misc]-[9dot]"
+	end
+
 end
