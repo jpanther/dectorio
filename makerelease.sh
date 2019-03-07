58d385202c67d33ede3b73548929a433eb078aca
James Panther <4462786+jpanther@users.noreply.github.com>
2019-03-07 11:09:26 +1100

    Update makerelease.sh

--- makerelease.sh
+++ makerelease.sh
@@ -8,19 +8,19 @@ modver=`cat info.json|jq -r .version`
 gitbranch=`git rev-parse --abbrev-ref HEAD`
 
 # Switch to master branch
-git checkout master
+#git checkout master
 
 # Merge changes from develop branch
-git merge --no-ff develop
+#git merge --no-ff 0.16
 
 # Create git tag for this version
 git tag "v$modver"
 
 # Prepare zip for Factorio native use and mod portal
-git archive --prefix "${modname}_$modver/" -o "../release/${modname}_$modver.zip" HEAD
+git archive --prefix "${modname}_$modver/" -o "/Users/james/Desktop/tmp/${modname}_$modver.zip" HEAD
 
 # Push tag to git
 git push origin "v$modver"
 
 # Switch back to original branch
-git checkout $gitbranch
+#git checkout $gitbranch
