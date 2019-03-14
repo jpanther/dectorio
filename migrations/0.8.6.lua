766b47ed89b8f46df9c8c0a2a82f68369ff567ca
James Panther <4462786+jpanther@users.noreply.github.com>
2019-03-14 11:13:15 +1100

    Remove dependency on Alien Biomes settings and use recipe instead - Fixes #88

--- migrations/0.8.6.lua
+++ migrations/0.8.6.lua
@@ -10,11 +10,13 @@ for index, force in pairs(game.forces) do
 		if tech["landfill"].researched then
 			rec["dect-base-water"].enabled = true
 			rec["dect-base-deepwater"].enabled = true
-			
+
 			-- When Alien Biomes is activated, the green variations of
 			-- the water aren't created by Dectorio.
-			if not settings.startup["alien-biomes-terrain-scale"] then
+			if rec["dect-base-water-green"] then
 				rec["dect-base-water-green"].enabled = true
+			end
+			if rec["dect-base-deepwater-green"] then
 				rec["dect-base-deepwater-green"].enabled = true
 			end
 		end
