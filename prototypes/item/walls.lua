e09cbb0d4c88ef4d675c4cf1986746a85bbc53d1
James Panther <4462786+jpanther@users.noreply.github.com>
2020-01-25 15:28:25 +1100

    New Stone and Concrete gates - Fixes #69

--- prototypes/item/walls.lua
+++ prototypes/item/walls.lua
@@ -1,41 +1,53 @@
 -- item/walls
 
 if DECT.ENABLED["walls"] then
-
-	data:extend({
+	data:extend(
 		{
-			type = "item",
-			name = "dect-concrete-wall",
-			icon = "__Dectorio__/graphics/icons/concrete-wall.png",
-			icon_size = 32,
-			subgroup = "defensive-structure",
-			order = "a[d-concrete-wall]",
-			stack_size = DECT.CONFIG.SETTINGS["walls_stack_size"],
-			place_result = "dect-concrete-wall"
-		},
-		{
-			type = "item",
-			name = "dect-chain-wall",
-			icon = "__Dectorio__/graphics/icons/chain-wall.png",
-			icon_size = 32,
-			subgroup = "defensive-structure",
-			order = "a[c-chain-wall]",
-			stack_size = DECT.CONFIG.SETTINGS["walls_stack_size"],
-			place_result = "dect-chain-wall"
-		},
-		{
-			type = "item",
-			name = "dect-wood-wall",
-			icon = "__Dectorio__/graphics/icons/wood-wall.png",
-			icon_size = 32,
-			fuel_category = "chemical",
-			fuel_value = "12MJ",
-			subgroup = "defensive-structure",
-			order = "a[a-wood-wall]",
-			stack_size = DECT.CONFIG.SETTINGS["walls_stack_size"],
-			place_result = "dect-wood-wall"
+			{
+				type = "item",
+				name = "dect-concrete-wall",
+				icon = "__Dectorio__/graphics/icons/concrete-wall.png",
+				icon_size = 32,
+				subgroup = "defensive-structure",
+				order = "a[d-concrete-wall]",
+				stack_size = DECT.CONFIG.SETTINGS["walls_stack_size"],
+				place_result = "dect-concrete-wall"
+			},
+			{
+				type = "item",
+				name = "dect-chain-wall",
+				icon = "__Dectorio__/graphics/icons/chain-wall.png",
+				icon_size = 32,
+				subgroup = "defensive-structure",
+				order = "a[c-chain-wall]",
+				stack_size = DECT.CONFIG.SETTINGS["walls_stack_size"],
+				place_result = "dect-chain-wall"
+			},
+			{
+				type = "item",
+				name = "dect-wood-wall",
+				icon = "__Dectorio__/graphics/icons/wood-wall.png",
+				icon_size = 32,
+				fuel_category = "chemical",
+				fuel_value = "12MJ",
+				subgroup = "defensive-structure",
+				order = "a[a-wood-wall]",
+				stack_size = DECT.CONFIG.SETTINGS["walls_stack_size"],
+				place_result = "dect-wood-wall"
+			},
+			{
+				type = "item",
+				name = "dect-concrete-gate",
+				icon = "__base__/graphics/icons/gate.png",
+				icon_size = 64,
+				icon_mipmaps = 4,
+				subgroup = "defensive-structure",
+				order = "a[x-concrete-gate]",
+				place_result = "dect-concrete-gate",
+				stack_size = 50
+			}
 		}
-	})
+	)
 
 	-- Change base stone wall
 	local base_stone_wall = data.raw["item"]["stone-wall"]
@@ -45,4 +57,11 @@ if DECT.ENABLED["walls"] then
 	base_stone_wall.stack_size = DECT.CONFIG.SETTINGS["walls_stack_size"]
 	base_stone_wall.order = "a[b-stone-wall]"
 
+	-- Change base gate
+	local base_gate = data.raw["item"]["gate"]
+	base_gate.icon = "__Dectorio__/graphics/icons/stone-gate.png"
+	base_gate.icon_size = 64
+	base_gate.icon_mipmaps = 1
+	base_gate.stack_size = DECT.CONFIG.SETTINGS["walls_stack_size"]
+	base_gate.order = "a[w-stone-gate]"
 end
--- prototypes/technology/walls.lua
+++ prototypes/technology/walls.lua
@@ -1,50 +1,68 @@
 -- technology/walls
 
 if DECT.ENABLED["walls"] then
-
 	-- Change stone wall tech to basic walls
 	local base_stone_walls = data.raw["technology"]["stone-walls"]
 	base_stone_walls.icon = "__Dectorio__/graphics/technology/basic-walls.png"
 	base_stone_walls.icon_size = 128
 	table.insert(base_stone_walls.effects, {type = "unlock-recipe", recipe = "dect-wood-wall"})
 
-	data:extend({
+	data:extend(
 		{
-			type = "technology",
-			name = "dect-advanced-walls",
-			icon = "__Dectorio__/graphics/technology/advanced-walls.png",
-			icon_size = 128,
-			prerequisites = {"concrete", "stone-walls"},
-			unit = {
-				count = 150,
-				ingredients = {
-					{"automation-science-pack", 1},
-					{"logistic-science-pack", 1}
+			{
+				type = "technology",
+				name = "dect-advanced-walls",
+				icon = "__Dectorio__/graphics/technology/advanced-walls.png",
+				icon_size = 128,
+				prerequisites = {"military-2", "concrete", "stone-walls"},
+				unit = {
+					count = 150,
+					ingredients = {
+						{"automation-science-pack", 1},
+						{"logistic-science-pack", 1},
+						{"military-science-pack", 1}
+					},
+					time = 30
 				},
-				time = 20
+				effects = {
+					{
+						type = "unlock-recipe",
+						recipe = "dect-chain-wall"
+					},
+					{
+						type = "unlock-recipe",
+						recipe = "dect-concrete-wall"
+					},
+					{
+						type = "unlock-recipe",
+						recipe = "dect-concrete-wall-from-stone-wall"
+					}
+				},
+				order = "e-p-b-a"
 			},
-			effects = {
-				{
-					type = "unlock-recipe",
-					recipe = "dect-chain-wall"
+			{
+				type = "technology",
+				name = "dect-advanced-gates",
+				icon = "__base__/graphics/technology/gates.png",
+				icon_size = 128,
+				prerequisites = {"gates", "dect-advanced-walls"},
+				unit = {
+					count = 150,
+					ingredients = {
+						{"automation-science-pack", 1},
+						{"logistic-science-pack", 1},
+						{"military-science-pack", 1}
+					},
+					time = 45
 				},
-				{
-					type = "unlock-recipe",
-					recipe = "dect-concrete-wall"
+				effects = {
+					{
+						type = "unlock-recipe",
+						recipe = "dect-concrete-gate"
+					}
 				},
-				{
-					type = "unlock-recipe",
-					recipe = "dect-concrete-wall-from-stone-wall"
-				}
-			},
-			order = "e-p-b-a"
+				order = "e-p-b-a"
+			}
 		}
-
-	})
-
-	-- Change gates to require concrete-walls and unlock Hazard gate
-	local base_gates = data.raw["technology"]["gates"]
-	base_gates.prerequisites = {"dect-advanced-walls", "military-2"}
-	--table.insert(base_gates.effects, {type="unlock-recipe", recipe="dect-hazard-gate"})
-
+	)
 end
