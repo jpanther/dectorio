e85e58f9b1e44f41b510e67f1e6467a5c7a9addd
James Panther <4462786+jpanther@users.noreply.github.com>
2020-01-29 11:32:43 +1100

    Fix for flooring migration issue - Ref #152

--- migrations/0.8.2.lua
+++ migrations/0.8.2.lua
@@ -1,12 +1,11 @@
 for index, force in pairs(game.forces) do
-
 	force.reset_recipes()
 	force.reset_technologies()
 
 	local tech = force.technologies
 	local rec = force.recipes
 
-	if settings.startup["dectorio-flooring"].value then
+	if settings.startup["dectorio-concrete"].value then
 		if tech["concrete"].researched then
 			rec["dect-concrete-grid"].enabled = true
 		end
