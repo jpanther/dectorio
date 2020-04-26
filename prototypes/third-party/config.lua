835cebdeed93edc5a1c0821cf4bada0525c04d08
James Panther <4462786+jpanther@users.noreply.github.com>
2020-04-26 14:33:29 +1000

    Add waterfill mode setting - Fixes #139

--- prototypes/third-party/config.lua
+++ prototypes/third-party/config.lua
@@ -6,8 +6,13 @@
 -- ---------------------------------
 if mods["alien-biomes"] then
 	-- Only supports blue water tiles
+	DECT.CONFIG.WATERFILL_MODES = {
+		["all"] = {"water", "deepwater"},
+		["shallow"] = {"water"},
+		["deep"] = {"deepwater"}
+	}
 	DECT.CONFIG.BASE_TILES = {}
-	DECT.CONFIG.BASE_WATER_TILES = {"water", "deepwater"}
+	DECT.CONFIG.BASE_WATER_TILES = DECT.CONFIG.WATERFILL_MODES[DECT.CONFIG.SETTINGS["waterfill_mode"]]
 	DECT.CONFIG.BASE_ROCKS = {}
 	DECT.CONFIG.BASE_TREES = {}
 	-- Remove the tile icons from signs
