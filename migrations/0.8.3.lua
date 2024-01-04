858915afe04a3d2e07442c948666b1ec27e0ba2e
Etienne Maheu <contact@etiennemaheu.com>
2024-01-04 17:10:11 -0500

    Update 0.8.3.lua to only enable recepies when they exist

--- migrations/0.8.3.lua
+++ migrations/0.8.3.lua
@@ -1,3 +1,10 @@
+function enableIfExists(table, key)
+	if (table[key] ~= nil)
+	then
+		table[key].enable = true
+	end
+end
+
 for index, force in pairs(game.forces) do
 
 	force.reset_recipes()
@@ -8,20 +15,20 @@ for index, force in pairs(game.forces) do
 
 	if settings.startup["dectorio-landscaping"].value then
 		if tech["dect-landscaping"].researched then
-			rec["dect-base-tree-01"].enabled = true
-			rec["dect-base-tree-02"].enabled = true
-			rec["dect-base-tree-03"].enabled = true
-			rec["dect-base-tree-04"].enabled = true
-			rec["dect-base-tree-05"].enabled = true
-			rec["dect-base-tree-06"].enabled = true
-			rec["dect-base-tree-07"].enabled = true
-			rec["dect-base-tree-08"].enabled = true
-			rec["dect-base-tree-09"].enabled = true
-			rec["dect-base-dead-dry-hairy-tree"].enabled = true
-			rec["dect-base-dead-grey-trunk"].enabled = true
-			rec["dect-base-dead-tree-desert"].enabled = true
-			rec["dect-base-dry-hairy-tree"].enabled = true
-			rec["dect-base-dry-tree"].enabled = true
+			enableIfExists(rec, "dect-base-tree-01")
+			enableIfExists(rec, "dect-base-tree-02")
+			enableIfExists(rec, "dect-base-tree-03")
+			enableIfExists(rec, "dect-base-tree-04")
+			enableIfExists(rec, "dect-base-tree-05")
+			enableIfExists(rec, "dect-base-tree-06")
+			enableIfExists(rec, "dect-base-tree-07")
+			enableIfExists(rec, "dect-base-tree-08")
+			enableIfExists(rec, "dect-base-tree-09")
+			enableIfExists(rec, "dect-base-dead-dry-hairy-tree")
+			enableIfExists(rec, "dect-base-dead-grey-trunk")
+			enableIfExists(rec, "dect-base-dead-tree-desert")
+			enableIfExists(rec, "dect-base-dry-hairy-tree")
+			enableIfExists(rec, "dect-base-dry-tree")
 		end
 	end
 end
